ef6570b3c3c902b0a79f8677e6c62623a040e3f6
Thiago Margarida <tmargarida@indeed.com>
2022-01-13 21:09:20 -0800

    add webmentions

--- src/_11ty/filters.js
+++ src/_11ty/filters.js
@@ -94,8 +94,8 @@ module.exports = {
   },
   isOwnWebmention: (webmention) => {
     const urls = [
-      'https://sia.codes',
-      'https://twitter.com/thegreengreek'
+      'https://geekosaur.com',
+      'https://twitter.com/FalconSensei'
     ]
     const authorUrl = webmention.author ? webmention.author.url : false
     // check if a given URL is part of this site.
