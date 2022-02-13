32723afe0b16ef25ea62fab62aa57ff2b9961fa6
Thiago Margarida <tmargarida@indeed.com>
2022-02-13 13:58:06 -0800

    only save cache on local build

--- src/_11ty/helpers.js
+++ src/_11ty/helpers.js
@@ -20,6 +20,9 @@ function readFromCache(cacheFilePath) {
 
 // save combined webmentions in cache file
 function writeToCache(data, cacheFilePath, descriptor) {
+  // Don't actually write to cache on dev or cloudflare
+  if (process.env.ELEVENTY_ENV !== "devbuild") return;
+
   const dir = "src/_cache";
   const fileContent = JSON.stringify(data, null, 2);
   // create cache folder if it doesnt exist already
