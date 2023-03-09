0a61a01e9ff2edf5115ea0a4ff0172e6e7f6a8a2
Falcon <falcon@falcon.moe>
2023-03-09 12:49:53 -0800

    update

--- src/_11ty/helpers.js
+++ src/_11ty/helpers.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
-// const fetch = require("node-fetch");
+
 const Image = require("@11ty/eleventy-img");
 const metadata = require("../_data/metadata.json");
 const cheerio = require("cheerio");
-
+const fetch = require("node-fetch");
 // const IMG_CACHE_FILE_PATH = "src/_cache/images.json";
 const external = /https?:\/\/((?:[\w\d-]+\.)+[\w\d]{2,})/i;
 // Matches bookmark links that are not inline
