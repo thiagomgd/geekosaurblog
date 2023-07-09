827467b7c7bfc0d54cb3934731651c849cbbd0d8
Falcon <falcon@falcon.moe>
2023-07-09 09:51:21 -0700

    update

--- src/_data/mylinks.js
+++ src/_data/mylinks.js
@@ -1,32 +1,6 @@
 const { getMastodonPostsForConfig } = require("../_11ty/helpers");
 const groupBy = require("lodash/groupBy");
 
-// // TODO: use my own fuctions for READ and WRITE
-// const readFromCache = (config) => {
-// 	if (fs.existsSync(config.cacheLocation)) {
-// 		const cacheFile = fs.readFileSync(config.cacheLocation);
-// 		return JSON.parse(cacheFile.toString());
-// 	}
-// 	// no cache found.
-// 	return { lastFetched: null, posts: [] };
-// };
-
-// const writeToCache = (config, data) => {
-// 	const dir = '.cache';
-// 	const fileContent = JSON.stringify(data, null, 2);
-// 	// create cache folder if it doesnt exist already
-// 	if (!fs.existsSync(dir)) {
-// 		fs.mkdirSync(dir);
-// 	}
-// 	// write data to cache json file
-// 	fs.writeFile(config.cacheLocation, fileContent, (err) => {
-// 		if (err) throw err;
-// 		console.log(
-// 			`>>> ${data.posts.length} mastodon posts in total are now cached in ${config.cacheLocation}`
-// 		);
-// 	});
-// };
-
 module.exports = async function () {
   const isProduction = process.env.ELEVENTY_ENV !== "development";
   const options = [
@@ -37,7 +11,6 @@ module.exports = async function () {
       removeSyndicates: ["geekosaur.com"],
       cacheLocation: "src/_cache/linksuniverseodon.json",
       removeTags: true,
-      // attributeFilter: ["embed", "card"],
       type: "links",
     },
   ];
@@ -48,14 +21,9 @@ module.exports = async function () {
     const toots = await getMastodonPostsForConfig(config);
 
     posts = posts.concat(toots);
-    // console.log('sizes: toots', toots.length, posts.length);
-    // console.log('****', config.host, '******')
   }
 
-  console.log(groupBy(posts, "linksPost"));
-  return groupBy(posts, "linksPost");
-  // sort again, but whatever...
-  // return posts
-  //   .sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
-  //   .reverse();
+  const grouped = groupBy(posts, "linksPost");
+  console.log(">>> Pending Links: ", grouped[undefined]?.length);
+  return grouped;
 };
